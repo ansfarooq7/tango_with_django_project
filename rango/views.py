fa84d5169073b218d7796b8027ea941d18708018
Ans Farooq <2390370f@student.gla.ac.uk>
2020-01-30 23:27:16 +0000

    added about template

--- rango/views.py
+++ rango/views.py
@@ -11,4 +11,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page." + '<a href="/rango/">Index</a>')
+    context_dict = {'boldmessage': 'This tutorial has been put together by Ans Farooq.'}
+    return render(request, 'rango/about.html', context=context_dict)
